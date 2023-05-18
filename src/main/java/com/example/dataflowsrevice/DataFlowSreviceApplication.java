13d5318f44c29e4b0640ba36df6c6b11ccfbe3dd
Alexander <a.kan4988@gmail.com>
2023-05-18 16:15:07 +0300

    initial commit

---
+++ src/main/java/com/example/dataflowsrevice/DataFlowSreviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataflowsrevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFlowSreviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataFlowSreviceApplication.class, args);
+    }
+
+}
